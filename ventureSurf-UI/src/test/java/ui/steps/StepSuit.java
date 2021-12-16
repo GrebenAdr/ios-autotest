f3295e294e9d1c69f496baa8d45e3d905bf5aad7
Agrebenkov <Agrebenkov@croc.ru>
2021-12-16 10:06:53 +0300

    test

--- ventureSurf-UI/src/test/java/ui/steps/StepSuit.java
+++ ventureSurf-UI/src/test/java/ui/steps/StepSuit.java
@@ -1,5 +1,6 @@
 package ui.steps;
 
+import io.qameta.allure.Allure;
 import org.testng.Assert;
 import ui.pages.*;
 
@@ -19,13 +20,18 @@ public class StepSuit {
         // TODO: 29.11.2021 найти способ сброса кешей или переустановки приложения, либо делать разлогин каждый раз перед закуском тестов
 //        if (loginElements.laFirstText.getText()){
 
+        Allure.step("Очистка поля");
         loginElements.getLoginField().clear();
+        Allure.step("Ввод телефона");
         loginElements.getLoginField().sendKeys("+15552222222");
+        Allure.step("Клик на чекбокс");
         loginElements.getLabelAgree().click();
+        Allure.step("Клик на кнопку Continue");
         loginElements.getSubmitBtn().click();
+        Allure.step("Ввод пароля");
         loginElements.getPassField().sendKeys("123123");
+        Allure.step("Пароль введен");
         Assert.assertNotNull(loginElements.laLabel.getText(), "Ошибка логина");
-
 //        Assert.assertEquals(loginElements.laLabel.getText(), "VentureSurf", "Ошибка логина");
 //        }
     }
@@ -33,9 +39,12 @@ public class StepSuit {
     public void logout() {
         // TODO: 10.12.2021 Не видит id. Либо не тот указал, либо не там указал в приложении
 //        tabBar.getTabBarProfileBtn().click();
+        Allure.step("Клик на кнопку с настройками");
         profile.getProfileSettingsBtn().click();
         // TODO: 10.12.2021 Можно добавить скролл(но не обязательно - работает и без него)
+        Allure.step("Клик на кнопку logout");
         profile.getSignOutBtn().click();
+        Allure.step("Кнопка logout нажата");
         // TODO: 10.12.2021 Поиск работает. Немного переделать assert
         Assert.assertNotNull(loginElements.laFirstText.getText(), "Ошибка логаута");
     }
